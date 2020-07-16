f4b927daf5364237eb3902d4e21b2874256811d6
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-07-16 15:56:08 -0600

    Exercise 1.11

---
+++ chapter_1/1.2/1.11.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+; Recursive function
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+(f 5)
+
+; Iterative function
+(define (iterative-f n)
+  (define (f-iter a b c count)
+    (cond ((< n 3) n)
+          ((<= count 0) a)
+          (else (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1)))))
+    (f-iter 2 1 0 ( - n 2)))
+
+(iterative-f 5)
+(iterative-f 40)
