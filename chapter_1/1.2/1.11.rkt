d961fbc6ad783185617ec22c6dfa5325e122a290
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-07-18 14:37:23 -0600

    Fixed a logical operator.

--- chapter_1/1.2/1.11.rkt
+++ chapter_1/1.2/1.11.rkt
@@ -12,11 +12,12 @@
 
 ; Iterative function
 (define (iterative-f n)
-  (define (f-iter a b c count)
+  (define (f-iter a b c count) 
     (cond ((< n 3) n)
-          ((<= count 0) a)
-          (else (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1)))))
-    (f-iter 2 1 0 ( - n 2)))
+          ((= count 0) a)
+          (else (f-iter
+                 (+ a (* 2 b) (* 3 c)) a b (- count 1)))))
+    (f-iter 2 1 0 ( - n 2))) ; initialize
 
-(iterative-f 5)
+(iterative-f 3)
 (iterative-f 40)
